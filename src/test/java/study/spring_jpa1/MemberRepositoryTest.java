4174425c5a095de65f80a59a0023af6f386fe7cd
Yongki Kim <kimdev0206@gmail.com>
2024-09-12 17:54:37 +0900

    add: 섹션2. 프로젝트 환경설정

---
+++ src/test/java/study/spring_jpa1/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.spring_jpa1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+  @Autowired
+  MemberRepository memberRepository;
+
+  @Test
+  @Transactional
+  public void testMember() {
+    // given
+    Member member = new Member();
+    member.setName("member");
+
+    // when
+    Long saveId = memberRepository.save(member);
+    Member findMember = memberRepository.find(saveId);
+
+    // then
+    assertThat(findMember.getId()).isEqualTo(member.getId());
+  }
+}
---
+++ src/test/java/study/spring_jpa1/SpringJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_jpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
